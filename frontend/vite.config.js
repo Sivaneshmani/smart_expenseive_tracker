c6127b7f923bd90920c0615e930175319bb7d39f
Sivanesh M <138895999+Sivaneshmani@users.noreply.github.com>
2025-04-28 09:03:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'  // Import autoprefixer
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: './', // Important for correct path resolution on Vercel
 })
